d37d9ef2cd601557b2f1aa61fcd85b0549e7feae
jasminlmm <30959327+jasminlmm@users.noreply.github.com>
2017-08-12 16:27:59 -0400

    Update thetour.js

--- thetour.js
+++ thetour.js
@@ -2,20 +2,41 @@ var vrHomePage = vRViewPlayer('#tour', {
     image: 'assets/jasmin.jpg',
     is_stereo: false,
 });
+
 window.addEventListener('load', vrHomePage);
 
 vrHomePage.on('ready', function(event) {
- vrHomePage.addHotspot('creating-hotspot-1', {
-     pitch: 0,
-     yaw: 0,
-     radius:0.10,
-     distance: 1
- });
+    vrHomePage.addHotspot('creating-hotspot-1', {
+        pitch: 0,
+        yaw: 0,
+        radius:0.10,
+        distance: 1
+    });
+    vrHomePage.addHotspot('creating-hotspot-2', {
+         pitch: 10,
+         yaw: 40,
+         radius:0.10,
+         distance: 1
+    }); 
+    
+    vrHomePage.addHotspot('creating-hotspot-3', {
+        pitch: 67,
+        yaw: 100,
+        radius:0.10,
+        distance: 1
+    });
+  
+    vrHomePage.addHotspot('creating-hotspot-4', {
+        pitch: 87,
+        yaw: 11,
+        radius:0.10,
+        distance: 1
+    });
+    vrHomePage.addHotspot('creating-hotspot-5', {
+        pitch: 15,
+        yaw: 20,
+        radius:0.10,
+        distance: 1
+    });
 });
 
-vrHomePage.on('click', function(event) {
-    console.log('register all clicks events');
-    if (event.id == 'creating-hotspot-1') {
-        alert("welcome to the upper west side")
-    }
-});
